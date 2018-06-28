90a40ac6ad86eddf3c1b906a9540d7eaf0e4f1e8
abnercrack <abnercrack@163.com>
2018-06-28 14:59:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,18 +35,22 @@ function compose (middleware) {
     let index = -1
     // 返回dispatch执行结果
     return dispatch(0)
-   
-    function dispatch (i) {
+    function dispatch (i) {  
       // 判断如果 i 小于索引 抛出异常
-      if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+      if (i <= index) return Promise.reject(new Error('next() called multiple times'))  
+      // 执行完一次索引值将会更新所以重新赋值
       index = i
+      // 从新的数组下标中取出中间件
       let fn = middleware[i]
+      // 如果到了最后一个 则设置fn等于next ？ next是undefined
       if (i === middleware.length) fn = next
-      
+      // 如果是undefined 直接返回一个 成功状态的promise 
       if (!fn) return Promise.resolve()
       try {
+        // 返回一个成功状态的promise对象 调用中间件函数 传入上下文 递归调用dispatch 执行下一个中间价
         return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
       } catch (err) {
+        // 如果执行过程中又错误则返回一个错误状态的promise
         return Promise.reject(err)
       }
     }
