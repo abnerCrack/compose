466f72d842474084851dc7ec5ede6e2af58308d8
abnercrack <abnercrack@163.com>
2018-06-28 14:14:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ module.exports = compose
  * @return {Function}
  * @api public
  */
-
 function compose (middleware) {
+  // 判断中间件是否是一个数组
   if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+  // 遍历数组 如有非function类型 抛异常
   for (const fn of middleware) {
     if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
   }
@@ -27,16 +28,21 @@ function compose (middleware) {
    * @return {Promise}
    * @api public
    */
-
+  // 函数柯里化 
   return function (context, next) {
     // last called middleware #
+    // 设置默认索引为 -1
     let index = -1
+    // 返回dispatch执行结果
     return dispatch(0)
+   
     function dispatch (i) {
+      // 判断如果 i 小于索引 抛出异常
       if (i <= index) return Promise.reject(new Error('next() called multiple times'))
       index = i
       let fn = middleware[i]
       if (i === middleware.length) fn = next
+      
       if (!fn) return Promise.resolve()
       try {
         return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
